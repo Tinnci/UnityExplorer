f31cda11aa3b8d2664052009a437d85850dd70d1
shiso <shisoratsu@icloud.com>
2026-05-27 03:05:45 +0800

    Use backported UniverseLib compatibility layer

--- build.ps1
+++ build.ps1
@@ -43,7 +43,7 @@ Remove-Item $Path/Microsoft.Extensions.Logging.Abstractions.dll
 New-Item -Path "$Path" -Name "Mods" -ItemType "directory" -Force
 Move-Item -Path $Path/UnityExplorer.ML.IL2CPP.net6preview.interop.dll -Destination $Path/Mods -Force
 New-Item -Path "$Path" -Name "UserLibs" -ItemType "directory" -Force
-Move-Item -Path $Path/UniverseLib.IL2CPP.Interop.ML.dll -Destination $Path/UserLibs -Force
+Move-Item -Path $Path/UniverseLib.ML.IL2CPP.Interop.dll -Destination $Path/UserLibs -Force
 # (create zip archive)
 Remove-Item $Path/../UnityExplorer.MelonLoader.IL2CPP.net6preview.interop.zip -ErrorAction SilentlyContinue
 Compress-Archive -Path .\$Path\* -DestinationPath $Path/../UnityExplorer.MelonLoader.IL2CPP.net6preview.interop.zip
@@ -120,7 +120,7 @@ Remove-Item $Path/UnityExplorer.BIE.IL2CPP.CoreCLR.deps.json
 New-Item -Path "$Path" -Name "plugins" -ItemType "directory" -Force
 New-Item -Path "$Path" -Name "plugins/sinai-dev-UnityExplorer" -ItemType "directory" -Force
 Move-Item -Path $Path/UnityExplorer.BIE.IL2CPP.CoreCLR.dll -Destination $Path/plugins/sinai-dev-UnityExplorer -Force
-Move-Item -Path $Path/UniverseLib.IL2CPP.Interop.dll -Destination $Path/plugins/sinai-dev-UnityExplorer -Force
+Move-Item -Path $Path/UniverseLib.BIE.IL2CPP.Interop.dll -Destination $Path/plugins/sinai-dev-UnityExplorer -Force
 # (create zip archive)
 Remove-Item $Path/../UnityExplorer.BepInEx.IL2CPP.CoreCLR.zip -ErrorAction SilentlyContinue
 Compress-Archive -Path .\$Path\* -DestinationPath $Path/../UnityExplorer.BepInEx.IL2CPP.CoreCLR.zip
